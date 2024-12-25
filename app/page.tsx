950499b716a5c25f7b0e309656965bcd8c563349
Thithut <junior29958@gmail.com>
2024-12-25 15:47:59 +0700

    add admin fuctionality

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import PatientForm from "@/components/forms/PatientForm";
-// import { PasskeyModal } from "@/components/PasskeyModal";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const { admin } = await searchParams;
@@ -10,7 +10,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* {isAdmin && <PasskeyModal />} */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
